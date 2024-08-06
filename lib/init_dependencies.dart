0adcbb9135aa7c156a540c06d18fffd4420df908
Faheem <mfaheemamin4797@gmail.com>
2024-08-06 14:09:42 +0500

    added bloc observer and get it dependencies

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:sumify_clean/core/network/connection_checker.dart';
+import 'package:sumify_clean/features/authentication/data/datasources/auth_remote_datasource.dart';
+import 'package:sumify_clean/features/authentication/data/repositories/auth_repository_impl.dart';
+import 'package:sumify_clean/features/authentication/domain/repositories/auth_repository.dart';
+import 'package:sumify_clean/features/authentication/domain/usecases/current_user.dart';
+import 'package:sumify_clean/features/authentication/domain/usecases/login_user.dart';
+import 'package:sumify_clean/features/authentication/domain/usecases/signup_user.dart';
+import 'package:sumify_clean/firebase_options.dart';
+
+part 'init_dependencies.main.dart';
---
+++ lib/init_dependencies.main.dart
@@ -0,0 +1,56 @@
+part of 'init_dependencies.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  final firebase = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // final supabase = await Supabase.initialize(
+  //   url: AppSecrets.supabaseUrl,
+  //   anonKey: AppSecrets.supabaseAnonKey,
+  // );
+  // Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
+  // serviceLocator.registerLazySingleton(() => Hive.box(name: 'blogs'));
+
+  serviceLocator.registerLazySingleton(() => firebase);
+  serviceLocator
+      .registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+  serviceLocator.registerLazySingleton(() => FirebaseFirestore.instance);
+  serviceLocator.registerLazySingleton(() => FirebaseStorage.instance);
+
+  serviceLocator.registerFactory(() => InternetConnection());
+  //core
+  // serviceLocator
+  //   ..registerFactory<SignoutRemoteDataSource>(
+  //       () => SignoutRemoteDataSourceImpl(firebaseAuth: serviceLocator()))
+  //   ..registerFactory<SignoutRepository>(
+  //       () => SignoutRepositoryImpl(serviceLocator(), serviceLocator()))
+  //   ..registerFactory(() => UserSignout(serviceLocator()));
+  // serviceLocator.registerLazySingleton(() => AppUserCubit());
+  serviceLocator.registerFactory<ConnectionChecker>(
+      () => ConnectionCheckerImpl(serviceLocator()));
+
+  await _initAuth();
+}
+
+Future<void> _initAuth() async {
+  serviceLocator
+        ..registerFactory<AuthRemoteDatasource>(
+            () => AuthRemoteDatasourceImpl(firebaseAuth: serviceLocator()))
+        ..registerFactory<AuthRepository>(() => AuthRepositoryImpl(
+            connectionChecker: serviceLocator(),
+            authRemoteDatasource: serviceLocator()))
+        ..registerFactory(() => SignupUser(authRepository: serviceLocator()))
+        ..registerFactory(() => LoginUser(authRepository: serviceLocator()))
+        // ..registerFactory(() => ForgotPassword(authRepository: serviceLocator()))
+        ..registerFactory(() => CurrentUser(authRepository: serviceLocator()))
+      // ..registerLazySingleton(() => AuthBloc(
+      //       signupUser: serviceLocator(),
+      //       loginUser: serviceLocator(),
+      //       forgotPassword: serviceLocator(),
+      //       currentUser: serviceLocator(),
+      //     ))
+      ;
+}
