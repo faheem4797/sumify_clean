9a6f49be77cd2888b349b96e619ecfa167fd3542
Faheem <mfaheemamin4797@gmail.com>
2024-08-17 04:29:44 +0500

    Remove app_secrets.dart and update .gitignore

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const String serviceId = 'service_9tz4peu';
-  static const String templateId = 'template_jkoixvw';
-  static const String userId = 'teMSbzGxAR-uv1HhS';
-}
