50220176f5e625c69e7972547b9866d82d40a7a6
Faheem <mfaheemamin4797@gmail.com>
2024-09-15 00:41:05 +0500

    permission request test going on

---
+++ test/core/utils/request_permission_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:sumify_clean/core/utils/request_permission.dart';
+
+class MockPermission extends Mock implements Permission {}
+
+void main() {
+  late MockPermission mockPermission;
+  setUp(() {
+    mockPermission = MockPermission();
+  });
+
+  test(
+    'should return true when permission is already granted',
+    () async {
+      // Arrange
+      when(() => mockPermission.isGranted).thenAnswer((_) async => true);
+
+      // Act
+      final result = await requestPermission(mockPermission);
+
+      // Assert
+      verify(() => mockPermission.isGranted);
+      expect(result, isTrue);
+    },
+  );
+}
